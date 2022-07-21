1d2ec849f89f41e13af711256dda667788146309
Dylan Bowman - Vardy Central <Dylan.Bowman@petervardy.com>
2022-07-21 12:44:45 +0100

    added filtering for selectors

--- src/ScraperForm.jsx
+++ src/ScraperForm.jsx
@@ -1,23 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+
 
 const ScraperForm = () => {
 
+    const [brand, setBrand] = useState()
+
     useEffect(() => {
       populateBrandFields()
-      populateModelFields()
-    })
+    },[])
   
     return (
       <div className="App">
-        <select name="brand" id="brand">brand</select>
+        <select name="brand" id="brand" onChange={(e) => {populateModelFields(e.target.value, setBrand)}}>brand</select>
         <select name="model" id="model"><option>model</option></select>
-  
+        <button>Scrape</button>
       </div>
     );
 }
 
 const populateBrandFields = () => {
     let selector = document.getElementById("brand")
+    selector.innerHTML = ""
     let list = ["porsche","bmw"]
     list.forEach(element => {
       let option = document.createElement("option")
@@ -26,9 +29,14 @@ const populateBrandFields = () => {
     });
 }
   
-  const populateModelFields = () => {
+  const populateModelFields = (model, setBrand) => {
+    var bmwlist =  ["1 Series", "2 Series"]
+    var porschelist = ["Cayenne", "Panamera"] 
+
+    setBrand(model)
     let selector = document.getElementById("model")
-    let list =  ["1 Series", "2 Series"]
+    selector.innerHTML = ""
+    let list = model === "bmw" ? bmwlist : porschelist
     list.forEach(element => {
       let option = document.createElement("option")
       option.innerHTML = element
