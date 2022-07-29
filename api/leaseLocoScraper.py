b375fc7a302087c51f2bde7fc25bea5bcba32dca
Dylan Bowman - Vardy Central <Dylan.Bowman@petervardy.com>
2022-07-29 16:22:54 +0100

    selenium added to project

--- api/leaseLocoScraper.py
+++ api/leaseLocoScraper.py
@@ -1,17 +1,24 @@
+from time import sleep, time
 import requests 
 #Imports the beautiful soup library for scraping
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
 
 
 def scrape():
+    path = "./venv/chromedriver.exe"
+    driver = webdriver.Chrome(path)
     url = "https://leaseloco.com/car-leasing/search"
     response = requests.get(url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.content,"html.parser")
-        div = soup.find("main", class_="container")
-        print(div)
-        return div
+        contentWrapper = soup.find("div", class_="row main m-0")
+        containerSearchResults = contentWrapper.find("div", class_="row p-0 m-0 mb-5")
+        div = containerSearchResults.find("div")
+        driver.get("http://google.com")
+        return "t"
     elif response.status_code == 403:
         print("forbidden")
     elif response.status_code == 404:
