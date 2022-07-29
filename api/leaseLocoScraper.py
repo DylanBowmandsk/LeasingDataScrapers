279385a4a91cea612f0d072818c6abd514b977cc
Dylan Bowman - Vardy Central <Dylan.Bowman@petervardy.com>
2022-07-29 14:17:58 +0100

    started work on scraper

--- api/leaseLocoScraper.py
+++ api/leaseLocoScraper.py
@@ -1,3 +1,17 @@
 import requests 
 #Imports the beautiful soup library for scraping
 from bs4 import BeautifulSoup
+
+
+def scrape(make,range):
+    url = "https://leaseloco.com/car-leasing/search"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content,"html.parser")
+        rows = collateData(soup,make,range)
+        return rows
+    elif response.status_code == 403:
+        print("forbidden")
+    elif response.status_code == 404:
+        print("not found")
