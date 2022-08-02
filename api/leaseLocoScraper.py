d60d38db238ee270a3976825b8c0c186d7593017
Dylan Bowman - Vardy Central <Dylan.Bowman@petervardy.com>
2022-08-02 15:40:17 +0100

    variables output in ll

--- api/leaseLocoScraper.py
+++ api/leaseLocoScraper.py
@@ -19,11 +19,17 @@ def scrape():
     driver.get(url)
     waitTimer = WebDriverWait(driver, 1).until(
     ec.presence_of_element_located((By.CLASS_NAME, "link--result-row")))
-    listLink = driver.find_elements(By.CLASS_NAME, "link--result-row")
+    listLink = driver.find_elements(By.XPATH, "//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a")
+    for idx, link in enumerate(listLink):
+        name = link.find_element(By.CLASS_NAME, "text-main").get_attribute("innerHTML")
+        price = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[4]/p[1]").get_attribute("innerHTML")
+        upfront = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[4]/p[2]/mark").get_attribute("innerHTML")
+        profile = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[2]/div[2]/div[2]/p[1]/mark").get_attribute("innerHTML")
+        mileage = upfront = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[2]/div[2]/div[2]/p[2]/mark").get_attribute("innerHTML")
+        print(name, price, upfront, profile, mileage)
+        
 
-    for link in listLink:
-        name = link.find_element(By.CLASS_NAME, "text-main")
-        print(name.get_attribute("innerHTML"))
+
     
     response = requests.get(url)
     if response.status_code == 200:
@@ -32,3 +38,5 @@ def scrape():
         print("forbidden")
     elif response.status_code == 404:
         print("not found")
+
+
