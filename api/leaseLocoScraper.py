1b47421177945fdd0d3aa0ce6160dcc5bdd836d5
Dylan Bowman - Vardy Central <Dylan.Bowman@petervardy.com>
2022-08-02 16:57:42 +0100

    helpful comments

--- api/leaseLocoScraper.py
+++ api/leaseLocoScraper.py
@@ -1,4 +1,3 @@
-from time import sleep, time
 import requests 
 #Imports the beautiful soup library for scraping
 from bs4 import BeautifulSoup
@@ -9,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 
 
-
+# global scrape function for lease loco
 def scrape():
     url = "https://leaseloco.com/car-leasing/search"
     path = "./venv/chromedriver.exe"
@@ -17,8 +16,11 @@ def scrape():
     options.headless = True
     driver = webdriver.Chrome(executable_path=path, options=options)
     driver.get(url)
-    waitTimer = WebDriverWait(driver, 1).until(
-    ec.presence_of_element_located((By.CLASS_NAME, "link--result-row")))
+
+    #waits for the page to load until list item appears
+    waitTimer = WebDriverWait(driver, 0.5).until(ec.presence_of_element_located((By.CLASS_NAME, "link--result-row")))
+
+    #main chunk that finds elements to extract data
     listLink = driver.find_elements(By.XPATH, "//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a")
     for idx, link in enumerate(listLink):
         name = link.find_element(By.CLASS_NAME, "text-main").get_attribute("innerHTML")
@@ -28,9 +30,6 @@ def scrape():
         mileage = upfront = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[2]/div[2]/div[2]/p[2]/mark").get_attribute("innerHTML")
         print(name, price, upfront, profile, mileage)
         
-
-
-    
     response = requests.get(url)
     if response.status_code == 200:
         return "api"
