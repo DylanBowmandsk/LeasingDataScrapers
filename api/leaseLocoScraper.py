94be8c4dc0902836f96e2d24254ef3df47e40b4a
Dylan Bowman - Vardy Central <Dylan.Bowman@petervardy.com>
2022-08-03 16:07:38 +0100

    progress

--- api/leaseLocoScraper.py
+++ api/leaseLocoScraper.py
@@ -12,7 +12,7 @@ from selenium.common.exceptions import NoSuchElementException
 # global scrape function for lease loco
 def scrape(make, model):
     url = "https://leaseloco.com/car-leasing/search"
-    driver = setupDriver(url)
+    driver = setupDriver(url, make, model)
     rows = getElements(driver, make, model)
         
     response = requests.get(url)
@@ -23,28 +23,25 @@ def scrape(make, model):
     elif response.status_code == 404:
         print("not found")
 
-def setupDriver(url):
+def setupDriver(url, make, model):
     path = "./venv/chromedriver.exe"
     options = Options()
     options.headless = True
     driver = webdriver.Chrome(executable_path=path, options=options)
     driver.get(url)
     driver.set_window_size(1024, 768)
-    setMakeModel(driver)
+    setMakeModel(driver, make, model)
     #waits for the page to load until list item appears
     waitTimer = WebDriverWait(driver, 3).until(ec.presence_of_element_located((By.CLASS_NAME, "link--result-row")))
     
 
     return driver
 
-def setMakeModel(driver):
-    makeButton = driver.find_element(By.CLASS_NAME, "add-filter--manufacturers")
-    makeButton.click()
-    waitTimer = WebDriverWait(driver, 3).until(ec.presence_of_element_located((By.CLASS_NAME, "button--manufacturers--audi")))
-    makesSelector = driver.find_element(By.CLASS_NAME, "button--manufacturers--audi")
-    makesSelector.click()
+def setMakeModel(driver, make, model):
+    searchBar = driver.find_element(By.CLASS_NAME, "search--input")
+    searchBar.sendkeys("test")
 
-
+    print(searchBar)
 
 def getElements(driver, make, model):
     listLink = driver.find_elements(By.XPATH, "//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a")
@@ -60,6 +57,7 @@ def getElements(driver, make, model):
             term = profile[1]+" Months"
             mileage = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[2]/div[2]/div[2]/p[2]/mark").get_attribute("innerHTML").replace("k", "000") + " p/a"
             totalLease = int(profile[1]) * float(price.replace("£",""))
+
             rows.append({"make": make+ " " + model,
             "price" : price+" p/m",
             "mileage" : mileage,
@@ -76,7 +74,7 @@ def getElements(driver, make, model):
             upfront = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[5]/p[2]/mark").get_attribute("innerHTML")
             profile = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[3]/div[2]/div[2]/p[1]/mark").get_attribute("innerHTML").split("+")
             initialTerm = profile[0]+" Months"
-            term = int(profile[1])+" Months"
+            term = profile[1]+" Months"
             mileage = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[3]/div[2]/div[2]/p[2]/mark").get_attribute("innerHTML")
             totalLease = int(profile[1]) * float(price.replace("£",""))
 
