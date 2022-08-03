75ed49be760d2d86d97f670c2bc5bc4d7b9fa0c8
Dylan Bowman - Vardy Central <Dylan.Bowman@petervardy.com>
2022-08-03 10:26:43 +0100

    fixed string formatting

--- api/leaseLocoScraper.py
+++ api/leaseLocoScraper.py
@@ -22,23 +22,9 @@ def scrape():
     #waits for the page to load until list item appears
     waitTimer = WebDriverWait(driver, 0.5).until(ec.presence_of_element_located((By.CLASS_NAME, "link--result-row")))
 
-    #main chunk that finds elements to extract data
-    listLink = driver.find_elements(By.XPATH, "//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a")
-    for idx, link in enumerate(listLink):
-        try:
-            name = link.find_element(By.CLASS_NAME, "text-main").get_attribute("innerHTML")
-            price = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[3]/div[2]/div[3]/div/div[2]/a[{idx+1}]/div/div[4]/p[1]").get_attribute("innerHTML")
-            upfront = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[4]/p[2]/mark").get_attribute("innerHTML")
-            profile = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[2]/div[2]/div[2]/p[1]/mark").get_attribute("innerHTML")
-            mileage = upfront = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[2]/div[2]/div[2]/p[2]/mark").get_attribute("innerHTML")
-        except NoSuchElementException:
-            name = link.find_element(By.CLASS_NAME, "text-main").get_attribute("innerHTML")
-            price = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[3]/div[2]/div[3]/div/div[2]/a[{idx+1}]/div/div[5]/p[1]").get_attribute("innerHTML")
-                       
-            upfront = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[5]/p[2]/mark").get_attribute("innerHTML")
-            profile = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[3]/div[2]/div[2]/p[1]/mark").get_attribute("innerHTML")
-            mileage = upfront = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[3]/div[2]/div[2]/p[2]/mark").get_attribute("innerHTML")
-        print(name, price, upfront, profile, mileage)
+    rows = getElements(driver)
+
+    print(rows)
         
     response = requests.get(url)
     if response.status_code == 200:
@@ -48,4 +34,26 @@ def scrape():
     elif response.status_code == 404:
         print("not found")
 
-
+def getElements(driver):
+    listLink = driver.find_elements(By.XPATH, "//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a")
+    rows = []
+    for idx, link in enumerate(listLink):
+        try:
+            name = link.find_element(By.CLASS_NAME, "text-main").get_attribute("innerHTML")
+            price = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[3]/div[2]/div[3]/div/div[2]/a[{idx+1}]/div/div[4]/p[1]").get_attribute("innerHTML")+"p/m"
+            upfront = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[4]/p[2]/mark").get_attribute("innerHTML")
+            profile = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[2]/div[2]/div[2]/p[1]/mark").get_attribute("innerHTML").split("+")
+            initialTerm = profile[0]+" Months"
+            term = profile[1]
+            mileage = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[2]/div[2]/div[2]/p[2]/mark").get_attribute("innerHTML").replace("k", "000") + " p/a"
+            rows.append([name,price,upfront,initialTerm,term,mileage])
+        except NoSuchElementException:
+            name = link.find_element(By.CLASS_NAME, "text-main").get_attribute("innerHTML")
+            price = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[3]/div[2]/div[3]/div/div[2]/a[{idx+1}]/div/div[5]/p[1]").get_attribute("innerHTML")
+            upfront = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[5]/p[2]/mark").get_attribute("innerHTML")
+            profile = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[3]/div[2]/div[2]/p[1]/mark").get_attribute("innerHTML").split("+")
+            initialTerm = profile[0]
+            term = profile[1]
+            mileage = driver.find_element(By.XPATH, f"//*[@id='__next']/main/div/div[2]/div[3]/div[3]/div/div[2]/a[{idx+1}]/div/div[3]/div[2]/div[2]/p[2]/mark").get_attribute("innerHTML")
+            rows.append([name,price,upfront,initialTerm,term,mileage])
+    return rows
