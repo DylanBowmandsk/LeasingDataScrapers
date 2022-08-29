000cc28c443a72a5ea101e1959bcc697797c1875
Dylan Bowman - Vardy Central <Dylan.Bowman@petervardy.com>
2022-08-29 16:22:25 +0100

    refining search

--- src/ModelRow.jsx
+++ src/ModelRow.jsx
@@ -1,13 +1,22 @@
 const ModelRow = ({name, term, mileage, leasingPrice, selectPrice, locoPrice, derivative}) => {
     return(
         <div>
+            <p className="inline-block">{name}</p>
+            <p className="block mb-10">{derivative}</p>
             <div className="flex flex-row">
-                <p className="inline-block">{name}</p>
-                <p>{derivative}</p>
-                <p>{selectPrice}</p>
-                <p>{leasingPrice}</p>
-                <p>{locoPrice}</p>
-                <p>{term}</p>
+                <div className="mr-20">
+                    <p>Select Leasing</p>
+                    <p>{selectPrice}</p>
+                </div>
+                <div className="mr-20">
+                    <p>Loco Leasing</p>
+                    <p>{locoPrice}</p>
+                </div>
+                <div className="mr-20 mb-20">
+                    <p>Leasing.com</p>
+                    <p>{leasingPrice}</p>
+                </div>
+
             </div>
         </div>
     )
